66cb364d995c67eb25ffe9184026c92c92396cc3
oyjf11 <oyjf_@hotmail.com>
2024-12-09 22:30:47 +0800

    vueuse useWindowSize

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,10 +1,13 @@
 import { computed } from 'vue'; // 计算属性
-import {PC_DEVICE_WIDTh} from '../constants'
+import {PC_DEVICE_WIDTh} from '../constants';
+import { useWindowSize } from '@vueuse/core';
+
+const {width} = useWindowSize();
 
 /**
  * 依据屏幕宽度 判断当前是否为移动端设备
  */
 
 export const isMobileTerminal = computed(() => {
-    return document.documentElement.clientWidth < PC_DEVICE_WIDTh;
+    return width.value < PC_DEVICE_WIDTh;
 })
