9c6214a2cac4e411ce207ea52b07c9f51e60d784
oyjf11 <oyjf_@hotmail.com>
2024-12-15 22:30:16 +0800

    rem字体自适应屏幕宽度

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -11,3 +11,22 @@ const {width} = useWindowSize();
 export const isMobileTerminal = computed(() => {
     return width.value < PC_DEVICE_WIDTh;
 })
+
+/**
+ * 判断当前是否为移动设备
+ */
+// export const isMobileTerminal = computed(() => {
+//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//      navigator.userAgent
+//   )
+// })
+
+export const useREM = () => {
+    const MAX_FONT_SIZE = 40
+    document.addEventListener('DOMContentLoaded', () => {
+      const htmlEl = document.documentElement
+      let fontSize = window.innerWidth / 10
+      fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+      htmlEl.style.fontSize = fontSize + 'px'
+    })
+  }
